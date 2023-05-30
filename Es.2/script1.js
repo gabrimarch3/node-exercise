ac85e1fe75652d64e4d211aeeb18ceaefbf3d63d
gabrimarch3 <gabrimarche@protonmail.com>
2023-05-30 16:42:04 +0200

    completed exercise

---
+++ Es.2/script1.js
@@ -0,0 +1,5 @@
+const sum = (num1, num2) => {
+    return `la somma è ${num1 + num2}`;
+}
+
+module.exports = sum;
---
+++ Es.2/script2.js
@@ -0,0 +1,3 @@
+const script1 = require("./script1");
+
+console.log(script1(2, 2));
