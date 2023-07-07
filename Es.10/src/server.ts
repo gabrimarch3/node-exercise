01f2728bcc32ccc3bdb63d1848366bed2aa6ae36
gabrimarch3 <gabrimarche@protonmail.com>
2023-07-07 06:21:30 +0200

    completed ex.10

---
+++ Es.10/src/server.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+
+const app = express()
+const port = 3000;
+
+app.use(morgan("dev"))
+app.use(express.json());
+
+type Planet = {
+    id: number,
+    name: string,
+  };
+
+  type Planets = Planet[];
+
+  let planets: Planets = [
+    {
+      id: 1,
+      name: "Earth"
+    },
+    {
+      id: 2,
+      name: "Mars"
+    },
+  ];
+
+app.get('/api/planets', (req, res) => {
+  res.status(200).json(planets)
+})
+
+app.get('/api/planets/:id', (req, res) => {
+  const {id} = req.params;
+  const planet = planets.find(item => item.id === Number(id))
+  res.status(200).json(planet)
+})
+
+app.post('/api/planets', (req, res) => {
+  const { id, name } = req.body;
+  const newPlanet = { id, name }
+  planets = [...planets, newPlanet]
+  console.log(planets)
+  res.status(201).json({msg: "Il pianeta è stato creato"})
+})
+
+app.put('/api/planets/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  planets = planets.map((planet) => planet.id === Number(id) ? {...planet, name} : planet)
+  console.log(planets)
+  res.status(200).json({msg: 'Il pianeta è stato aggiornato'})
+})
+
+app.delete('/api/planets/:id', (req, res) => {
+  const { id } = req.params;
+  planets = planets.filter(planet => planet.id !== Number(id));
+  console.log(planets)
+  res.status(200).json({msg: 'Il pianeta è stato cancellato'})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
